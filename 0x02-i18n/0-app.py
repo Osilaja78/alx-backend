4c79eee4e738027d17c4dfeef570ff7b582d163c
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2024-02-07 04:58:53 +0100

    fixed pycodestyle error for task 0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -10,6 +10,7 @@ def hello_world():
 
     return render_template('0-index.html')
 
+
 if __name__ == "__main__":
     """ Main Function """
     app.run(host='0.0.0.0', port=5000)
--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -18,6 +18,7 @@ def hello_world():
 
     return render_template('0-index.html')
 
+
 if __name__ == "__main__":
     """ Main Function """
     app.run(host='0.0.0.0', port=5000)
